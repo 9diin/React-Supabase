583de6dda716506219acc00e75e1819f623a2b17
9diin <weartstudio@naver.com>
2025-10-09 11:13:57 +0900

    ☘️ [feature] CRUD 기능 중 CRD 기능 개발 완료

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router";
+import { useNavigate, useSearchParams } from "react-router";
 import { useAuthStore } from "@/stores";
 import { AppDraftsDialog, AppSidebar } from "../components/common";
 import { SkeletonHotTopic, SkeletonNewTopic } from "../components/skeleton";
@@ -13,13 +13,26 @@ import { NewTopicCard } from "@/components/topics";
 function App() {
     const user = useAuthStore((state) => state.user);
     const navigate = useNavigate();
+    const [searchParams, setSearchParams] = useSearchParams();
+    const category = searchParams.get("category") || "";
 
     const [topics, setTopics] = useState<Topic[]>([]);
+
+    const handleCategoryChange = (value: string) => {
+        if (value === category) return; // 선택된 항목 재선택시 무시
+        if (value === "") setSearchParams({});
+        else setSearchParams({ category: value });
+    };
 
     // 발행된 토픽 조회
     const fetchTopics = async () => {
         try {
-            const { data: topics, error } = await supabase.from("topic").select("*").eq("status", "publish");
+            const query = supabase.from("topic").select("*").eq("status", "publish");
+
+            if (category && category.trim() !== "") {
+                query.eq("category", category);
+            }
+            const { data: topics, error } = await query;
 
             if (error) {
                 toast.error(error.message);
@@ -70,7 +83,7 @@ function App() {
 
     useEffect(() => {
         fetchTopics();
-    }, []);
+    }, [category]);
 
     return (
         <main className="w-full h-full min-h-[720px] flex p-6 gap-6">
@@ -89,9 +102,11 @@ function App() {
                 </AppDraftsDialog>
             </div>
             {/* 카테고리 사이드바 */}
-            <AppSidebar />
+            <div className="hidden lg:block lg:min-w-60 lg:w-60 lg:h-full">
+                <AppSidebar category={category} setCategory={handleCategoryChange} />
+            </div>
             {/* 토픽 콘텐츠 */}
-            <section className="flex-1 flex flex-col gap-12">
+            <section className="w-full lg:w-[calc(100%-264px)] flex flex-col gap-12">
                 {/* 핫 토픽 */}
                 <div className="w-full flex flex-col gap-6">
                     <div className="flex flex-col gap-1">
@@ -101,7 +116,7 @@ function App() {
                         </div>
                         <p className="md:text-base text-muted-foreground">지금 가장 주목받는 주제들을 살펴보고, 다양한 관점의 인사이트를 얻어보세요.</p>
                     </div>
-                    <div className="grid grid-cols-4 gap-6">
+                    <div className="w-full flex items-center gap-6 overflow-auto">
                         <SkeletonHotTopic />
                         <SkeletonHotTopic />
                         <SkeletonHotTopic />
@@ -118,7 +133,7 @@ function App() {
                         <p className="md:text-base text-muted-foreground">새로운 시선으로, 새로운 이야기를 시작하세요. 지금 바로 당신만의 토픽을 작성해보세요.</p>
                     </div>
                     {topics.length > 0 ? (
-                        <div className="min-h-120 grid grid-cols-2 gap-6">
+                        <div className="flex flex-col min-h-120 md:grid md:grid-cols-2 gap-6">
                             {topics
                                 .sort((a, b) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime())
                                 .map((topic: Topic) => {
